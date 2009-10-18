bd1d3f426bc6a81cc530e4baf6601c3f552d4283
Haakon Sporsheim <haakon.sporsheim@gmail.com>
2009-10-18 13:42:42 +0200

    [deploy] Deploy targets and props files to HSBuild subdir instead of MSBuild root.

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
 @mkdir "%ProgramFiles%\MSBuild\HSBuild"
 @copy "%~dp0\msbuild\tasks\bin\Release\HSBuildTasks.dll" "%ProgramFiles%\MSBuild\HSBuild"
-@copy "%~dp0\msbuild\*.targets" "%ProgramFiles%\MSBuild"
-@copy "%~dp0\msbuild\*.props" "%ProgramFiles%\MSBuild"
+@copy "%~dp0\msbuild\*.targets" "%ProgramFiles%\MSBuild\HSBuild"
+@copy "%~dp0\msbuild\*.props" "%ProgramFiles%\MSBuild\HSBuild"
 @xcopy /I /Y /E "%~dp0\msbuild\templates" "%ProgramFiles%\MSBuild\HSBuild\templates"
 
