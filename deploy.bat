5dd29c36ac4fca5ee51f7cadc1372a511e3d9f87
Haakon Sporsheim <haakon.sporsheim@gmail.com>
2009-10-18 13:42:12 +0200

    [msbuild/templates] Add templates subdir containing a template for config.h which also will be deployed when running deploy.bat.

--- deploy.bat
+++ deploy.bat
@@ -2,4 +2,5 @@
 @copy "%~dp0\msbuild\tasks\bin\Release\HSBuildTasks.dll" "%ProgramFiles%\MSBuild\HSBuild"
 @copy "%~dp0\msbuild\*.targets" "%ProgramFiles%\MSBuild"
 @copy "%~dp0\msbuild\*.props" "%ProgramFiles%\MSBuild"
+@xcopy /I /Y /E "%~dp0\msbuild\templates" "%ProgramFiles%\MSBuild\HSBuild\templates"
 
