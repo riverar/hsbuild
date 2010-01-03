aa7671daf8bfda0acbcbc8a2bac9000e50a800bf
Haakon Sporsheim <haakon.sporsheim@gmail.com>
2010-01-03 23:43:11 +0100

    [msbuild/tasks] Add SetEnv and GetEnv tasks.

---
+++ msbuild/tasks/environment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Framework;
+
+namespace HSBuild.Tasks
+{
+    public class SetEnv : Task
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Value { get; set; }
+        [Output]
+        public string OutputEnvironmentVariable
+        {
+            get { return Environment.GetEnvironmentVariable(Name); }
+        }
+
+        public override bool Execute()
+        {
+            Environment.SetEnvironmentVariable(Name, Value);
+            return true;
+        }
+    }
+
+    public class GetEnv : Task
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required, Output]
+        public string Value { get; set; }
+
+        public override bool Execute()
+        {
+            Value = Environment.GetEnvironmentVariable(Name);
+            return true;
+        }
+    }
+}
