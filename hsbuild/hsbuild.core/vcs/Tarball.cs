e237824c7dac5be1d6bda9d678f8ca238225f50e
Haakon Sporsheim <haakon.sporsheim@gmail.com>
2010-01-10 21:44:21 +0100

    [hsbuild] Implement missing Tarball support.

--- hsbuild/hsbuild.core/vcs/Tarball.cs
+++ hsbuild/hsbuild.core/vcs/Tarball.cs
@@ -16,7 +16,9 @@
 // along with HSBuild.  If not, see <http://www.gnu.org/licenses/>.
 //
 using System;
+using System.IO;
 using HSBuild.Core;
+using HSBuild.Tasks;
 
 namespace HSBuild.VCS
 {
@@ -45,32 +47,65 @@ namespace HSBuild.VCS
 
     public class TarballBranch : Branch
     {
-        public TarballBranch(TarballRepository repo, string mod, string rev, string root)
-            : base(mod, rev, root)
+        public TarballBranch(TarballRepository repo, string mod, string id, string ver, string root)
+            : base(GetTarballName(id, ver), null, root)
         {
+            if (mod == null)
+                throw new ArgumentNullException("mod");
+            if (id == null)
+                throw new ArgumentNullException("id");
+
             m_repo = repo;
+            m_uri = new Uri(new Uri(m_repo.HRef), mod);
+        }
+
+        private static string GetTarballName(string name, string version)
+        {
+            if (name == null)
+                throw new ArgumentNullException("name");
+
+            return (version == null) ? name : string.Format("{0}-{1}", name, version);
         }
 
         public override bool Exists(bool remote)
         {
-            throw new NotImplementedException();
+            if (remote)
+                throw new NotImplementedException("Remote exists");
+            else
+                return Directory.Exists(BranchRoot);
         }
 
         protected override void Checkout(ITaskQueue taskQueue)
         {
-            throw new NotImplementedException();
+            string local;
+            if (m_uri.IsFile)
+            {
+                local = m_uri.LocalPath;
+            }
+            else
+            {
+                HttpTask task = new HttpTask(m_uri);
+                taskQueue.QueueTask(task);
+                local = task.LocalFile;
+            }
+
+            taskQueue.QueueTask(new ArchiveTask(local, m_uri.AbsolutePath, m_checkoutRoot));
         }
 
         protected override void Update(ITaskQueue taskQueue)
         {
-            throw new NotImplementedException();
+            if (!Exists(false))
+                Checkout(taskQueue);
         }
 
         protected override void ApplyPatch(ITaskQueue taskQueue, Patch patch, string local_patch)
         {
-            throw new NotImplementedException();
+            // FIXME: change Tarball::ApplyPatch to NOT use git apply!
+            string gitEXE = Path.Combine(Environment.GetEnvironmentVariable("ProgramFiles"), "Git\\bin\\git.exe");
+            taskQueue.QueueTask(new ConsoleTask(gitEXE, new string[] { "apply", "-p" + patch.Strip.ToString(), "\"" + local_patch + "\"" }, BranchRoot));
         }
 
         private TarballRepository m_repo;
+        private Uri m_uri;
     }
 }
