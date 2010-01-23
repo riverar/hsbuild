d70da4d6740e7af7810749602e915e34a41d7aaa
Haakon Sporsheim <haakon.sporsheim@gmail.com>
2010-01-23 15:43:59 +0100

    [hsbuild] Add email to copyright header where it is missing.

--- hsbuild/hsbuild.core/ModuleBranch.cs
+++ hsbuild/hsbuild.core/ModuleBranch.cs
@@ -1,6 +1,6 @@
 ﻿// HSBuild.Core - ModuleBranch
 //
-// Copyright (C) 2009-2010 Haakon Sporsheim
+// Copyright (C) 2009-2010 Haakon Sporsheim <haakon.sporsheim@gmail.com>
 //
 // HSBuild is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
