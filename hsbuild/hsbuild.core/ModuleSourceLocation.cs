10493c066f650d6819b3daab2cc8ea992514ec36
Haakon Sporsheim <haakon.sporsheim@gmail.com>
2010-01-24 15:35:23 +0100

    [hsbuild] Add <tarball /> support as ModuleSourceLocation.

--- hsbuild/hsbuild.core/ModuleSourceLocation.cs
+++ hsbuild/hsbuild.core/ModuleSourceLocation.cs
@@ -16,8 +16,10 @@
 // along with HSBuild.  If not, see <http://www.gnu.org/licenses/>.
 //
 using System;
+using System.IO;
 using System.Xml;
 using System.Collections.Generic;
+using HSBuild.Tasks;
 
 namespace HSBuild.Core
 {
@@ -81,6 +83,81 @@ namespace HSBuild.Core
         private Repository m_repository;
     }
 
+    public class ModuleSourceTarball : ModuleSourceLocation
+    {
+        internal static ModuleSourceLocation ParseModuleSourceTarball(XmlNode xmlTarball, string moduleFallback)
+        {
+            XmlAttribute href = xmlTarball.Attributes["href"];
+            if (href == null || string.IsNullOrEmpty(href.Value))
+                throw new NotImplementedException("TODO: Error.. tarball must have href attribute!");
+
+            XmlAttribute version = xmlTarball.Attributes["version"];
+            XmlAttribute subdirectory = xmlTarball.Attributes["subdirectory"];
+            if (subdirectory == null || string.IsNullOrEmpty(subdirectory.Value))
+                subdirectory = xmlTarball.Attributes["subdir"];
+            XmlNodeList patches = xmlTarball.SelectNodes("patches/patch");
+
+            string checkoutdir;
+            if (subdirectory != null && !string.IsNullOrEmpty(subdirectory.Value))
+                checkoutdir = subdirectory.Value;
+            else if (version != null && !string.IsNullOrEmpty(version.Value))
+                checkoutdir = string.Format("{0}-{1}", moduleFallback, version.Value);
+            else
+                throw new Exception("Error: <tarball /> must have either subdirectory or version attribute.");
+
+            return new ModuleSourceTarball(new Uri(href.Value), moduleFallback,
+                checkoutdir, ParsePatches(patches));
+        }
+
+        private ModuleSourceTarball(Uri href, string module, string checkoutdir, Patch patchQueue)
+            : base(module, checkoutdir, patchQueue)
+        {
+            m_href = href;
+        }
+
+        internal override bool BindRepository(Dictionary<string, Repository> repos)
+        {
+            return true;
+        }
+
+        internal override void Update(ITaskQueue taskQueue, IOutputEngine output, Config config, bool onlyFirstTime)
+        {
+            string localDir = Path.Combine(config.CheckoutRoot, CheckoutDir);
+            if (Directory.Exists(localDir))
+                return;
+
+            string localTarball = DownloadFile(taskQueue, m_href);
+            taskQueue.QueueTask(new ArchiveTask(localTarball, m_href.AbsolutePath, config.CheckoutRoot));
+
+            Patch patch = PatchQueue;
+            while (patch != null)
+            {
+                string local = DownloadFile(taskQueue, patch.Uri);
+                ApplyPatch(taskQueue, patch, local, localDir);
+                patch = patch.Next;
+            }
+        }
+
+        private string DownloadFile(ITaskQueue taskQueue, Uri uri)
+        {
+            if (uri.IsFile)
+                return uri.LocalPath;
+
+            HttpTask task = new HttpTask(uri);
+            taskQueue.QueueTask(task);
+            return task.LocalFile;
+        }
+
+        private void ApplyPatch(ITaskQueue taskQueue, Patch patch, string localPatch, string path)
+        {
+            // FIXME: change Tarball::ApplyPatch to NOT use git apply!
+            string gitEXE = Path.Combine(Environment.GetEnvironmentVariable("ProgramFiles"), "Git\\bin\\git.exe");
+            taskQueue.QueueTask(new ConsoleTask(gitEXE, new string[] { "apply", "--whitespace=nowarn -p" + patch.Strip.ToString(), "\"" + localPatch + "\"" }, path));
+        }
+
+        private Uri m_href;
+    }
+
     public abstract class ModuleSourceLocation
     {
         internal static ModuleSourceLocation ParseSourceLocation(XmlElement xmlModule, string moduleFallback)
@@ -89,6 +166,9 @@ namespace HSBuild.Core
             {
                 if (node.Name.Equals("branch", StringComparison.OrdinalIgnoreCase))
                     return ModuleVCSBranch.ParseModuleVCSBranch(node, moduleFallback);
+
+                if (node.Name.Equals("tarball", StringComparison.OrdinalIgnoreCase))
+                    return ModuleSourceTarball.ParseModuleSourceTarball(node, moduleFallback);
             }
 
             return null;
