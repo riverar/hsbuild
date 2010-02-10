12e8cab63dcaa58f7992ff54ec44869ce12933f2
Haakon Sporsheim <haakon.sporsheim@gmail.com>
2010-02-11 00:06:00 +0100

    [wix] Deploy the x86 spesific version of HSBuild tool (hsbuild.exe).
    Since Toolset is deployed in 32bit compatible location on 64bit systems HSBuild needs to run on a 32bit VM.

--- deploy.bat
+++ deploy.bat
@@ -10,6 +10,6 @@
 @xcopy /I /Y /E "%~dp0\tools" "%ProgramFiles%\HSBuild\tools"
 
 @mkdir "%ProgramFiles%\HSBuild\bin"
-@copy "%~dp0\hsbuild\bin\Release\hsbuild.console.exe" "%ProgramFiles%\HSBuild\bin\hsbuild.exe"
-@copy "%~dp0\hsbuild\bin\Release\HSbuild.Core.dll" "%ProgramFiles%\HSBuild\bin"
-@copy "%~dp0\hsbuild\bin\Release\ICSharpCode.SharpZipLib.dll" "%ProgramFiles%\HSBuild\bin"
+@copy "%~dp0\hsbuild\bin\x86\Release\hsbuild.console.exe" "%ProgramFiles%\HSBuild\bin\hsbuild.exe"
+@copy "%~dp0\hsbuild\bin\x86\Release\HSbuild.Core.dll" "%ProgramFiles%\HSBuild\bin"
+@copy "%~dp0\hsbuild\bin\x86\Release\ICSharpCode.SharpZipLib.dll" "%ProgramFiles%\HSBuild\bin"
