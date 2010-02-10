47a7bd3b9c6ff2300aadfa3e49d8b723089364ed
Haakon Sporsheim <haakon.sporsheim@gmail.com>
2010-02-11 00:09:25 +0100

    [deploy.bat] Installing files %ProgramFiles(x86)% on 64bit systems.

--- deploy.bat
+++ deploy.bat
@@ -1,15 +1,17 @@
-@mkdir "%ProgramFiles%\MSBuild\HSBuild"
-@copy "%~dp0\msbuild\tasks\bin\Release\*.dll" "%ProgramFiles%\MSBuild\HSBuild"
-@copy "%~dp0\msbuild\*.targets" "%ProgramFiles%\MSBuild\HSBuild"
-@copy "%~dp0\msbuild\*.props" "%ProgramFiles%\MSBuild\HSBuild"
-@xcopy /I /Y /E "%~dp0\msbuild\Platform" "%ProgramFiles%\MSBuild\HSBuild\Platform"
-@xcopy /I /Y /E "%~dp0\msbuild\templates" "%ProgramFiles%\MSBuild\HSBuild\templates"
-@if exist "%ProgramFiles% (x86)" mklink /D "%ProgramFiles% (x86)\MSBuild\HSBuild" "%ProgramFiles%\MSBuild\HSBuild"
+@set PF=%ProgramFiles%
+@if not "%ProgramFiles(x86)%" == "" set PF=%ProgramFiles(x86)%
+@echo ProgramFiles set to %PF%
 
-@mkdir "%ProgramFiles%\HSBuild"
-@xcopy /I /Y /E "%~dp0\tools" "%ProgramFiles%\HSBuild\tools"
+@if not exist "%PF%\MSBuild\HSBuild" mkdir "%PF%\MSBuild\HSBuild"
+@if not exist "%PF%\HSBuild\bin" mkdir "%PF%\HSBuild\bin"
 
-@mkdir "%ProgramFiles%\HSBuild\bin"
-@copy "%~dp0\hsbuild\bin\x86\Release\hsbuild.console.exe" "%ProgramFiles%\HSBuild\bin\hsbuild.exe"
-@copy "%~dp0\hsbuild\bin\x86\Release\HSbuild.Core.dll" "%ProgramFiles%\HSBuild\bin"
-@copy "%~dp0\hsbuild\bin\x86\Release\ICSharpCode.SharpZipLib.dll" "%ProgramFiles%\HSBuild\bin"
+@copy "%~dp0\msbuild\tasks\bin\Release\*.dll" "%PF%\MSBuild\HSBuild"
+@copy "%~dp0\msbuild\*.targets" "%PF%\MSBuild\HSBuild"
+@copy "%~dp0\msbuild\*.props" "%PF%\MSBuild\HSBuild"
+@xcopy /I /Y /E "%~dp0\msbuild\Platform" "%PF%\MSBuild\HSBuild\Platform"
+@xcopy /I /Y /E "%~dp0\msbuild\templates" "%PF%\MSBuild\HSBuild\templates"
+
+@copy "%~dp0\hsbuild\bin\x86\Release\hsbuild.console.exe" "%PF%\HSBuild\bin\hsbuild.exe"
+@copy "%~dp0\hsbuild\bin\x86\Release\HSbuild.Core.dll" "%PF%\HSBuild\bin"
+@copy "%~dp0\hsbuild\bin\x86\Release\ICSharpCode.SharpZipLib.dll" "%PF%\HSBuild\bin"
+@xcopy /I /Y /E "%~dp0\tools" "%PF%\HSBuild\tools"
